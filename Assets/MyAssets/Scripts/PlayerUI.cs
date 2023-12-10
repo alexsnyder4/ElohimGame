0fe95eb0b5bddbff4707f425cb7b76859d6a65a3
alexsnyder4 <129345536+alexsnyder4@users.noreply.github.com>
2023-12-10 14:43:23 -0500

    added Character Panel

--- Assets/MyAssets/Scripts/PlayerUI.cs
+++ Assets/MyAssets/Scripts/PlayerUI.cs
@@ -10,6 +10,10 @@ public class PlayerUI : MonoBehaviour
     public PlayerData pd;
     public TMP_Text hp;
     public TMP_Text mana;
+    public GameObject charPanel;
+    public GameObject inventory;
+    public bool invActive = false;
+    public bool charActive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,4 +27,15 @@ public class PlayerUI : MonoBehaviour
         hp.text = pd.health + "/" + pd.maxHealth;
         mana.text = pd.mana + "/" + pd.maxMana;
     }
+    public void ToggleClickInventory()
+    {
+        inventory.SetActive(!invActive);
+        invActive = !invActive;
+    }
+    public void ToggleClickCharPanel()
+    {
+        charPanel.SetActive(!charActive);
+        charActive = !charActive;
+    }
+    
 }
