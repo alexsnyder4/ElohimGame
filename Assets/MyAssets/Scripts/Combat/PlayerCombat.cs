b7e0af466064642099ed1867b626d8f0410a53ca
alexsnyder4 <129345536+alexsnyder4@users.noreply.github.com>
2023-12-21 21:10:50 -0500

    Attack animation added, Left CLick to attack

---
+++ Assets/MyAssets/Scripts/Combat/PlayerCombat.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    private Animator animator;
+    public bool isInCombat = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        isInCombat = CheckIfInCombat();
+        UpdateLayerWeights();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))//Left Click
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    bool CheckIfInCombat()
+    {
+        return true;
+    }
+
+    void UpdateLayerWeights()
+    {
+        // Get the current layer weights
+        float combatLayerWeight = isInCombat ? 1.0f : 0.0f; // 1.0f when in combat, 0.0f otherwise
+
+        // Set the layer weights based on the combat state
+        animator.SetLayerWeight(0, 1 - combatLayerWeight); // Reduce influence of base layer when in combat
+        animator.SetLayerWeight(1, combatLayerWeight); // Increase influence of combat layer when in combat
+    }
+}
